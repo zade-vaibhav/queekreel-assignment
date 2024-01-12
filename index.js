d8f34cbca548113eaf27cf26f698a91bcd82c4a9
vaibhav <vaibhavzade802@gmail.com>
2024-01-12 19:17:17 +0530

    adding public folder and writing html

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express=require("express")
 
 const app=express();
 
-app.listen(3001,()=>{
+app.use(express.static('public'))
+
+app.get("/",(req,res)=>{
+    res.sendFile("index.html")
+})
+
+app.listen(5000,()=>{
     console.log('server is running')
 })
