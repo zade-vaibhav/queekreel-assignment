62121bd0d4c1bac22a360a6881f7390db5bc0067
vaibhav <vaibhavzade802@gmail.com>
2024-01-13 18:04:51 +0530

    try to update logic of ffmpeg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const multer=require("multer")
 const ffmpeg = require('fluent-ffmpeg');
-
+const path=require("path")
 
 
 
@@ -25,9 +25,49 @@ app.get("/",(req,res)=>{
 })
 
 app.post("/upload",upload.single('file'),(req,res)=>{
-   console.log(req.file.path)
+   console.log(req.file)
    console.log(req.body.fillerRemove)
    res.send(req.body)
+
+   const filePath=req.file.path;
+
+   const fileName=req.file.originalname
+
+   // Input audio file
+const FilePath = filePath;
+
+// Output audio file
+const outputFilePath = Date.now()+fileName+".mp3";
+console.log(outputFilePath)
+// Filler word to be removed
+const fillerWords=req.body.fillerRemove
+const fillerWord = fillerWords.split(",");
+console.log(fillerWord)
+
+// Create a filter to remove the filler word
+//  const filter= `arealmatch(s, '${fillerWord[0]}')`;
+// const filter = fillerWord.map(word => `arealmatch(s, '${word}')`).join(',')
+const filter = fillerWord.map(word => `areverse,astats=metadata=1:reset=1,ametadata=print:key=lavfi.astats.${word}.ratio:file=-[aout], anull[in]`).join(';');
+const command=ffmpeg(FilePath);
+
+
+command.audioFilter(`silencedetect=n=-50:d=0.5`) // Optional: Add an audio filter to enhance silence detection
+command.audioFilter('anullsrc=r=44100:cl=stereo')
+
+command.on('start', () => {
+    console.log('Trimming audio...');
+  })
+command.on('error',(err)=>{
+    console.log("an error takes place : "+err.massage)
+}).on("end",()=>{
+    res.download(path.join(__dirname,"public/uploads",outputFilePath))
+}).save(path.join(__dirname,"public/uploads",outputFilePath))
+
+
+
+
+
+
 
 })
 
